4dd92fa17e138454988757b7c58be55d6252dee7
kyial4 <ik.dreamer7@gmail.com>
2022-05-12 19:32:15 +0600

    comnn

--- app/src/main/java/com/geektech/java3_hw2_isakova_kyial/ThirdFragment.java
+++ app/src/main/java/com/geektech/java3_hw2_isakova_kyial/ThirdFragment.java
@@ -1,10 +1,8 @@
-
 package com.geektech.java3_hw2_isakova_kyial;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -13,32 +11,28 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class SecondFragment extends Fragment {
 
-    private TextView textview;
-    private Button btnClick2;
+public class ThirdFragment extends Fragment {
+    private TextView textview3;
+    private Button btnClick3;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        textview = view.findViewById(R.id.textview);
-        btnClick2=view.findViewById(R.id.btn_click2);
-        if (!textview.getText().toString().isEmpty()) {
-           String val= getArguments().getString("key1");
-           textview.setText(val);
+        return inflater.inflate(R.layout.fragment_third, container, false);
+        textview3 = view.findViewById(R.id.textview3);
+        btnClick3=  view.findViewById(R.id.btn_click3);
+        if (getArguments() != null) {
+            String val= getArguments().getString("key1");
+            textview3.setText(val);
         }
-        btnClick2.setOnClickListener(new View.OnClickListener() {
+        btnClick3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Bundle bundle=new Bundle();
-                bundle.putString("key1",textview.getText().toString());
+                bundle.putString("key1",textview3.getText().toString());
                 ThirdFragment fragment =new ThirdFragment();
                 fragment.setArguments(bundle);
                 requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).addToBackStack(null).commit();
